1f254bc19b6671b176debb1f05afb1a1f2729fd9
SandSnip3r <sandsnip3r@gmail.com>
2024-09-09 16:55:24 -0700

    Fix loading script

--- load/load.py
+++ load/load.py
@@ -23,15 +23,20 @@ def main(model_path):
   print(f'Policy net: {policy_net}')
 
   observation, info = env.reset()
+  observationTensor = common.observationToTensor(env, observation, device)
   print(env.render())
   print(f'Initial info: {info}')
+
+  res = policy_net(observationTensor)
+  print(res)
 
   done = False
 
   while not done:
-    action = common.select_action(env, observation, policy_net, device, eps_threshold=None, deterministic=True)
+    action = common.select_action(env, observationTensor, policy_net, device, eps_threshold=None, deterministic=True)
 
     observation, reward, terminated, truncated, info = env.step(action.item())
+    observationTensor = common.observationToTensor(env, observation, device)
     print(env.render())
     print(f'info: {info}')
 
