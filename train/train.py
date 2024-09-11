c83bcbb69ab587eee95c76737a70cec653a3c44e
Victor Stone <SandSnip3r@gmail.com>
2024-09-11 05:11:52 -0700

    Do not enable determinism by default when setting the seed

--- train/train.py
+++ train/train.py
@@ -28,29 +28,30 @@ class ReplayMemory(object):
   def __len__(self):
     return len(self.memory)
 
-def set_seed(seed, env):
+def set_seed(seed, env, determinism=False):
   # For Python's random module
   random.seed(seed)
-  
+
   # For NumPy's random module
   np.random.seed(seed)
-  
+
   # For PyTorch (CPU)
   torch.manual_seed(seed)
-  
+
   # For PyTorch (GPU)
   if torch.cuda.is_available():
     torch.cuda.manual_seed(seed)
     torch.cuda.manual_seed_all(seed)  # If using multi-GPU
 
-  # Ensure deterministic behavior in PyTorch (optional, but can slow down training)
-  torch.backends.cudnn.deterministic = True
-  torch.backends.cudnn.benchmark = False
-  torch.use_deterministic_algorithms(True)
-
   # Seed the environment
   env.action_space.seed(seed)
   env.reset(seed=seed)
+
+  if determinism:
+    # Ensure deterministic behavior in PyTorch (optional, but can slow down training)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+    torch.use_deterministic_algorithms(True)
 
 def main():
   device = common.getDevice()
